2193143efa26e53b3ce0a63cd04190454d60e18e
Josh Mu <mu@joshmu.com>
2020-03-11 11:43:59 +1000

    ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
+import { ThemeProvider } from '@material-ui/core'
 import { CssBaseline } from '@material-ui/core'
+import theme from './config/theme'
 
 import Header from './layout/Header'
 import Footer from './layout/Footer'
@@ -22,24 +24,26 @@ import styles from './styles.module.scss'
 function App() {
   return (
     <CssBaseline>
-      <div className={styles.app}>
-        <PlayerState>
-          <TodoState>
-            <Router>
-              <Header />
-              <Layout>
-                <Switch>
-                  <Route exact path="/" component={VideoPage} />
-                  <Route exact path="/notes" component={Notes} />
-                  <Route exact path="/about" component={AboutPage} />
-                  <Route component={VideoPage} />
-                </Switch>
-              </Layout>
-              <Footer />
-            </Router>
-          </TodoState>
-        </PlayerState>
-      </div>
+      <ThemeProvider theme={theme}>
+        <div className={styles.app}>
+          <PlayerState>
+            <TodoState>
+              <Router>
+                <Header />
+                <Layout>
+                  <Switch>
+                    <Route exact path="/" component={VideoPage} />
+                    <Route exact path="/notes" component={Notes} />
+                    <Route exact path="/about" component={AboutPage} />
+                    <Route component={VideoPage} />
+                  </Switch>
+                </Layout>
+                <Footer />
+              </Router>
+            </TodoState>
+          </PlayerState>
+        </div>
+      </ThemeProvider>
     </CssBaseline>
   )
 }
